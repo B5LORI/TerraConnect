debaf76a443031b4f077229261df53b818b96f04
B5LORI <38064475+B5LORI@users.noreply.github.com>
2022-02-27 10:32:31 +0200

    Delete lib/features/number_trivia_el directory

--- lib/features/number_trivia_el/domain/repositories/number_trivia_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../../../../core_el/error/failure.dart';
-import '../entities/number_trivia.dart';
-import 'package:dartz/dartz.dart';
-
-abstract class NumberTriviaRepository {
-  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
-}
-
